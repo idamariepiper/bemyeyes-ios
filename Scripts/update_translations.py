940ee8694488cfdbad1fc22ec7c566990d3bc681
Marcin Kuptel <marcinkuptel@gmail.com>
2015-01-30 12:04:18 +0100

    A new version of the CrowdIn script that puts all the AppStore.txt files in a separate folder.

--- Scripts/update_translations.py
+++ Scripts/update_translations.py
@@ -12,6 +12,8 @@ PROJECT_ID = "bemyeyes"
 ENDPOINT = "https://api.crowdin.com/api/project/{projectID}/download/{package}.zip?key={projectKey}"
 LANG_STATUS_ENDPOINT = "https://api.crowdin.com/api/project/{projectID}/status?key={projectKey}&jsonp"
 FILENAME = "languages.zip"
+APP_STORE_DESCRIPTION = "AppStore.txt"
+APP_STORE_DESCRIPTIONS_DIR = "app_store_descriptions"
 EXTRACTION_DIR = "extracted_languages"
 RENAME_RULES = {'zh-CN':'zh-Hant', 'zh-TW' : 'zh-Hans', 'ur-PK' : 'ur', 'es-ES' : 'es', 'sv-SE' : 'sv', 'pt-PT' : 'pt'}
 
@@ -20,6 +22,10 @@ def usage():
 
 
 def fetchLanguages(projectKey):
+	"""
+	This function fetches all translations in a zipped file
+	and returns the name of the downloaded file or an error.
+	"""
 	try:
 		languagesURL = ENDPOINT.format(projectID=PROJECT_ID, package="all", projectKey=projectKey)
 		(filename, headers) = urllib.urlretrieve(languagesURL, FILENAME)
@@ -30,6 +36,10 @@ def fetchLanguages(projectKey):
 	
 	
 def unzipFile(filename):
+	"""
+	Unzips the file containing translations.
+	Returns name of the directory where files were extracted.
+	"""
 	try:
 		file = ZipFile(filename)
 		file.extractall(EXTRACTION_DIR)
@@ -43,6 +53,10 @@ def unzipFile(filename):
 	
 	
 def renameAndRemoveDirectories(parentDir, renameRules, fullyTranslated):
+	"""
+	Renames folders by following the RENAME_RULES and adds the .lproj suffix.
+	Removes folders for languages that have not been fully translated.
+	"""
 	extractedDir = os.path.join(os.getcwd(), parentDir)
 	dirsList = [name for name in os.listdir(extractedDir)
 	            if os.path.isdir(os.path.join(extractedDir, name))]
@@ -55,13 +69,40 @@ def renameAndRemoveDirectories(parentDir, renameRules, fullyTranslated):
 				print("{0} -> {1}".format(dir, dirName))
 				os.rename(os.path.join(extractedDir, dir), os.path.join(extractedDir, dirName))
 			else:
-				pass
 				shutil.rmtree(os.path.join(extractedDir, dir))
 	except OSError as error:
 		print(error.strerror)
 
 
+def moveAppStoreDescriptions(dirName):
+	"""
+	Moves all AppStore.txt files into a separate directory.
+	"""
+	descriptionsDir = ''
+	try:
+		descriptionsDir = os.path.join(os.getcwd(), APP_STORE_DESCRIPTIONS_DIR)
+		os.mkdir(descriptionsDir)
+		
+		extractedDir = os.path.join(os.getcwd(), dirName)
+		dirsList = [name for name in os.listdir(extractedDir)
+				            if os.path.isdir(os.path.join(extractedDir, name))]
+		for dir in dirsList:
+			fromPath = os.path.join(extractedDir, dir, APP_STORE_DESCRIPTION)
+			toPath = os.path.join(descriptionsDir, dir, APP_STORE_DESCRIPTION)
+			newDir = os.path.join(descriptionsDir, dir)
+			os.mkdir(newDir)
+			shutil.move(fromPath, toPath)
+
+	except OSError as e:
+		print(e.strerror)
+		sys.exit(2)
+			
+
 def fetchLanguageStatus(projectKey):
+	"""
+	Fetches info about languages to determine which ones
+	have been fully translated.
+	"""
 	fullyTranslated = []
 	try:
 		statusURL = LANG_STATUS_ENDPOINT.format(projectID=PROJECT_ID, projectKey=projectKey)
@@ -79,11 +120,17 @@ def fetchLanguageStatus(projectKey):
 				fullyTranslated.append(status["code"])
 	return fullyTranslated
 
+
 def isAcceptable(progress):
+	"""
+	Function evaluating whether a translation is acceptable based
+	on its progress.
+	"""
 	if progress == 100:
 		return True
 	else:
 		return False
+
 
 if __name__ == "__main__":
 	try:
@@ -109,6 +156,7 @@ if __name__ == "__main__":
 		if filename:
 			dirName = unzipFile(filename)
 			renameAndRemoveDirectories(dirName, RENAME_RULES, fullyTranslated)
+			moveAppStoreDescriptions(dirName)
 		else:
 			print(exception.strerror)
 	
