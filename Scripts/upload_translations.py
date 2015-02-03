9d431f06a1abb2fcce3b69f2a000a8230aa80760
Marcin Kuptel <marcinkuptel@gmail.com>
2015-02-03 12:39:18 +0100

    Added options for either adding new files or updating them

--- Scripts/upload_translations.py
+++ Scripts/upload_translations.py
@@ -2,7 +2,12 @@
 
 """
 Usage:
-	upload_translations.py -p <project-key>
+	upload_translations.py -p <project-key> [-a|-u] 
+	
+	-a : Add files
+	-u : Update files
+	
+	Either -a or -u should be specified. Defaults to -a.
 """
 
 import requests, os, getopt, sys
@@ -14,7 +19,7 @@ PROJECT_ID = "bemyeyes-test-project"
 ####### Production
 #PROJECT_ID = "bemyeyes"
 
-UPDATE_FILE_URL = "https://api.crowdin.com/api/project/{projectID}/add-file?key={APIKey}"
+UPDATE_FILE_URL = "https://api.crowdin.com/api/project/{projectID}/{addOrUpdate}-file?key={APIKey}"
 PATH_TO_ENGLISH = "../BeMyEyes/Localization/en.lproj"
 
 def usage():
@@ -23,7 +28,7 @@ def usage():
 def p(message):
 	print("### {0} ###".format(message))
 
-def uploadFiles(APIKey):
+def uploadFiles(APIKey, mode):
 	englishDir = pathToEnglishTranslation()
 	fileList = fileListAt(englishDir, lambda x: x.endswith(".strings"))
 	
@@ -37,24 +42,24 @@ def uploadFiles(APIKey):
 		filesToUpload["files[{0}]".format(file)] = open(location, "r")
 		if len(filesToUpload) == maxFiles:
 			p("Uploading {0} files".format(len(filesToUpload)))
-			if not makeUploadRequest(filesToUpload, APIKey):
-				print("Upload unsuccessful! Status code {0}. Response: {1}".format(response.status_code, response.text))
-				return False
-			else:
+			if makeUploadRequest(filesToUpload, APIKey, mode):
 				filesToUpload = {}
+			else:
+				return False
 				
 	if len(filesToUpload):
 		p("Uploading {0} files".format(len(filesToUpload)))
-		success = makeUploadRequest(filesToUpload, APIKey)
-		if not success:
-			print("Upload unsuccessful! Status code {0}. Response: {1}".format(response.status_code, response.text))
-		else:
+		success = makeUploadRequest(filesToUpload, APIKey, mode)
+		if success:
 			p("Upload successful")
+		else:
+			return False
 	
 	return True
 
-def makeUploadRequest(filesToUpload, APIKey):
-	response = requests.post(uploadURL(PROJECT_ID, APIKey), files=filesToUpload)
+def makeUploadRequest(filesToUpload, APIKey, mode):
+	url = uploadURL(PROJECT_ID, APIKey, mode)
+	response = requests.post(url, files=filesToUpload)
 	if response.status_code != 200:
 		print("Upload unsuccessful! Status code {0}. Response: {1}".format(response.status_code, response.text))
 		return False
@@ -69,15 +74,21 @@ def fileListAt(directory, condition):
 			            if condition(name)]
 	return dirsList
 
-def uploadURL(projID, key):
-	return UPDATE_FILE_URL.format(projectID=projID, APIKey=key)
+def uploadURL(projID, key, mode):
+	if mode == 'a':
+		return UPDATE_FILE_URL.format(projectID=projID, APIKey=key, addOrUpdate="add")
+	elif mode == 'u':
+		return UPDATE_FILE_URL.format(projectID=projID, APIKey=key, addOrUpdate="update")
+	else:
+		return ''
+	
 
 ###### Main ######
 
 if __name__ == "__main__":
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "p:")
-		if len(opts) != 1:
+		opts, args = getopt.getopt(sys.argv[1:], "p:ua")
+		if len(opts) != 2:
 			usage()
 			sys.exit(2)
 	except getopt.GetoptError:
@@ -85,12 +96,17 @@ if __name__ == "__main__":
 		sys.exit(2)
 	else:
 		projectKey = ''
+		mode = 'a'
 		for opt, arg in opts:
 			if opt == "-p":
 				projectKey = arg
+			elif opt == "-u":
+				mode = 'u'
+			elif opt == "-a":
+				pass
 			else:
 				usage()
 				sys.exit(2)
 		
-	uploadFiles(projectKey)
+	uploadFiles(projectKey, mode)
 	
