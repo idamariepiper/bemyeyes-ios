a3364439e0f8a184d58f08d4cbd0ebd9bbeaa7ea
marcinkuptel <marcinkuptel@gmail.com>
2015-02-03 13:17:00 +0100

    A bit of cleanup and comments

--- Scripts/upload_translations.py
+++ Scripts/upload_translations.py
@@ -11,28 +11,39 @@ Usage:
 """
 
 import requests, os, getopt, sys
-from poster.encode import multipart_encode
 
 # Testing
 PROJECT_ID = "bemyeyes-test-project"
 
 ####### Production
 #PROJECT_ID = "bemyeyes"
-
+MAX_FILE_LIST_LENGTH = 20
+IGNORED_FILES = ["InfoPlist.strings"]
 UPDATE_FILE_URL = "https://api.crowdin.com/api/project/{projectID}/{addOrUpdate}-file?key={APIKey}"
 PATH_TO_ENGLISH = "../BeMyEyes/Localization/en.lproj"
 
 def usage():
 	print(__doc__)
 
+
 def p(message):
+	"""
+	Used to print distinguishable messages.
+	"""
 	print("### {0} ###".format(message))
 
+
 def uploadFiles(APIKey, mode):
+	"""
+	The heart of this script. This function looks for English translation files
+	and uploads them to CrowdIn.
+	
+	APIKey - key giving access to the CrowdIn API
+	mode - either 'a' (add files) or 'u' (update files)
+	"""
 	englishDir = pathToEnglishTranslation()
 	fileList = fileListAt(englishDir, lambda x: x.endswith(".strings"))
 	
-	maxFiles = 20
 	filesToUpload = {}
 	
 	p("Preparing for upload")
@@ -40,7 +51,7 @@ def uploadFiles(APIKey, mode):
 	for file in fileList:
 		location = os.path.join(englishDir, file)
 		filesToUpload["files[{0}]".format(file)] = open(location, "r")
-		if len(filesToUpload) == maxFiles:
+		if len(filesToUpload) == MAX_FILE_LIST_LENGTH:
 			p("Uploading {0} files".format(len(filesToUpload)))
 			if makeUploadRequest(filesToUpload, APIKey, mode):
 				filesToUpload = {}
@@ -57,7 +68,11 @@ def uploadFiles(APIKey, mode):
 	
 	return True
 
+
 def makeUploadRequest(filesToUpload, APIKey, mode):
+	"""
+	Function opening a connection to the CrowdIn server and uploading files.
+	"""
 	url = uploadURL(PROJECT_ID, APIKey, mode)
 	response = requests.post(url, files=filesToUpload)
 	if response.status_code != 200:
@@ -66,15 +81,25 @@ def makeUploadRequest(filesToUpload, APIKey, mode):
 	else:
 		return True
 		
+		
 def pathToEnglishTranslation():
 	return os.path.join(os.getcwd(), PATH_TO_ENGLISH)
 	
+	
 def fileListAt(directory, condition):
+	"""
+	Return a list of all elements in a given directory that 
+	fulfill the supplied condition.
+	"""
 	dirsList = [name for name in os.listdir(directory)
 			            if condition(name)]
 	return dirsList
 
+
 def uploadURL(projID, key, mode):
+	"""
+	We need a different URL depending on whether the user wants to add files or upload them.
+	"""
 	if mode == 'a':
 		return UPDATE_FILE_URL.format(projectID=projID, APIKey=key, addOrUpdate="add")
 	elif mode == 'u':
